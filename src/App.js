33b3aec51a026f4c5537958143bb21b02e74c93d
kylezhao101 <kylezhao101@gmail.com>
2023-08-12 16:48:31 -0700

    Implement search input for drink name, flavour, tags, and preparation

--- src/App.js
+++ src/App.js
@@ -1,38 +1,55 @@
 import './App.css';
 import DrinkCard from './components/DrinkCard';
-import React, {useState, useEffect} from 'react';
+import React, {useMemo, useRef, useState, useEffect} from 'react';
 import {collection, getDocs} from 'firebase/firestore';
 import {firestore} from './util/firebase'
-import {drinkData} from './util/testDrinkData'
+
 function App() {
 
   const [drinks, setDrinks] = useState([]);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     async function fetchDrinks(){
       const drinksCollection = collection(firestore, 'test-drinks');
-      const drinksSnapshotr = await getDocs(drinksCollection);
+      const drinksSnapshot = await getDocs(drinksCollection);
       const fetchedDrinks = [];
 
-      drinksSnapshotr.forEach((drinkDoc) => {
+      drinksSnapshot.forEach((drinkDoc) => {
         const drink = drinkDoc.data();
         fetchedDrinks.push(drink);
       });
 
       setDrinks(fetchedDrinks);
     }
-
     fetchDrinks();
   }, []);
 
+  const filteredDrinks = useMemo(() => {
+    const lowercaseQuery = query.toLowerCase();
+    return drinks.filter(drink => {
+      return (
+        drink.Name.toLowerCase().includes(lowercaseQuery) ||
+        drink.Flavour.some((flavour) => flavour.toLowerCase().includes(lowercaseQuery)) ||
+        drink.Tags.some((tag) => tag.toLowerCase().includes(lowercaseQuery)) || 
+        drink.Preparation.some((preparation) => preparation.toLowerCase().includes(lowercaseQuery))
+      );
+    });
+  }, [drinks, query]);
+
+
   return (
     <div className="App">
+      <input
+          value = {query}
+          onChange = {e => setQuery(e.target.value)}
+          type = "search"
+          placeholder = "search drinks"
+        />
       <div className='flex gap-2 flex-wrap'>
-        {drinks.map((drink, index) => (
+        {filteredDrinks.map((drink, index) => (
           <DrinkCard key={index} drink={drink}/>
         ))}
-        <DrinkCard drink={drinkData['drink_id_5']}/>
-        <DrinkCard drink={drinkData['drink_id_18']}/>
       </div>
     </div>
   );
