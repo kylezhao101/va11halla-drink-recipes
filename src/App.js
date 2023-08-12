7f5d11d428f291113d15f236653e154b2fc76acc
kylezhao101 <kylezhao101@gmail.com>
2023-08-11 17:11:50 -0700

    Add second placeholder drink data
    This drink uses 'optional' as a value for Karmotrine

--- src/App.js
+++ src/App.js
@@ -24,6 +24,29 @@ const drinkData = {
     ],
     "Price": 320,
     "Quote": "This drink does not represent the opinions of the Bar Pianists Union or its associates."
+  },
+  "drink_id_5": {
+    "Image": 'https://static.wikia.nocookie.net/va11halla/images/6/62/BlueFairy.png',
+    "Name": "Blue Fairy",
+    "Flavour": [
+      "Sweet"
+    ],
+    "Tags": [
+      "Girly",
+      "Soft"
+    ],
+    "Ingredients": {
+      "Adelhyde": 4,
+      "Bronson Extract": 0,
+      "Powdered Delta": 0,
+      "Flanergide": 1,
+      "Karmotrine": 'optional'
+    },
+    "Preparation": [
+      "aged and mixed"
+    ],
+    "Price": 170,
+    "Quote": "One of these will make all your teeth turn blue. Hope you brushed them well."
   }
 };
 
@@ -31,6 +54,7 @@ function App() {
   return (
     <div className="App">
       <DrinkCard drink={drinkData['drink_id_18']}/>
+      <DrinkCard drink={drinkData['drink_id_5']}/>
     </div>
   );
 }
