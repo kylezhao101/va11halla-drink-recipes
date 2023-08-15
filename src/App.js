031302c83328c88fa8f822f61413debee1351c00
kylezhao101 <kylezhao101@gmail.com>
2023-08-15 00:37:17 -0700

    Normalize query input for and match with multiple drink fields

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import DrinkCard from './components/DrinkCard';
-import React, {useMemo, useRef, useState, useEffect} from 'react';
+import React, {useMemo, useState, useEffect} from 'react';
 import {collection, getDocs} from 'firebase/firestore';
 import {firestore} from './util/firebase'
 
@@ -25,18 +25,14 @@ function App() {
     fetchDrinks();
   }, []);
 
-  const filteredDrinks = useMemo(() => {
-    const lowercaseQuery = query.toLowerCase();
-    return drinks.filter(drink => {
-      return (
-        drink.Name.toLowerCase().includes(lowercaseQuery) ||
-        drink.Flavour.some((flavour) => flavour.toLowerCase().includes(lowercaseQuery)) ||
-        drink.Tags.some((tag) => tag.toLowerCase().includes(lowercaseQuery)) || 
-        drink.Preparation.some((preparation) => preparation.toLowerCase().includes(lowercaseQuery))
-      );
-    });
-  }, [drinks, query]);
-
+const filteredDrinks = useMemo(() => {
+  const lowercaseQuery = query.toLowerCase().replace(/[^\w\s]/g, ''); // Remove non-alphanumeric characters
+  const queryWords = lowercaseQuery.split(/\s+/); // Split query into words
+  return drinks.filter(drink => {
+    const combinedText = `${drink.Name} ${drink.Flavour.join(' ')} ${drink.Tags.join(' ')} ${drink.Preparation.join(' ')}`;
+    return queryWords.every(word => combinedText.toLowerCase().includes(word));
+  });
+}, [drinks, query]);
 
   return (
     <div className="App">
