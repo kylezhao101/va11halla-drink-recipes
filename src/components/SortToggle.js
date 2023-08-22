dd890047569c510b20eebf57e611a8f54d9011e0
kylezhao101 <kylezhao101@gmail.com>
2023-08-22 13:32:21 -0700

    Refractor SortToggle component

--- src/components/SortToggle.js
+++ src/components/SortToggle.js
@@ -1,45 +1,33 @@
-import React from 'react';
-import '../App.css';
+function SortToggle({ selectedSort, setSelectedSort }) {
+  const options = [
+    { value: 'name', label: 'Name' },
+    { value: 'price', label: 'Price' },
+    { value: 'flavour', label: 'Flavour' }
+  ];
 
-function SortToggle({selectedSort, setSelectedSort}){
-    return(
-        <div className='flex gap-6 font-body'>
-          <p className='text-white'>Sort/group by</p>
-          <div>
-            <input
-              type='radio'
-              id='name'
-              value='name'
-              name='sort'
-              className='peer hidden'
-              onChange={() => setSelectedSort('name')}
-            />
-            <label htmlFor='name' className='block cursor-pointer text-red-interactive peer-checked:text-white duration-100'>Name</label>
-          </div>
-          <div>
-            <input
-              type='radio'
-              id='price'
-              value='price'
-              name='sort'
-              className='peer hidden'
-              onChange={() => setSelectedSort('price')}
-            />
-            <label htmlFor='price' className='block cursor-pointer text-red-interactive  peer-checked:text-white duration-100'>Price</label>
-          </div>
-          <div>
-            <input
-              type='radio'
-              id='flavour'
-              value='flavour'
-              name='sort'
-              className='peer hidden'
-              onChange={() => setSelectedSort('flavour')}
-            />
-            <label htmlFor='flavour' className='block cursor-pointer text-red-interactive  peer-checked:text-white duration-100'>Flavour</label>
-          </div>
+  return (
+    <div className='flex gap-6 font-body'>
+      <p className='text-white'>Sort</p>
+      {options.map((option) => (
+        <div key={option.value}>
+          <input
+            type='radio'
+            id={option.value}
+            value={option.value}
+            name='sort'
+            className='peer hidden'
+            onChange={() => setSelectedSort(option.value)}
+          />
+          <label
+            htmlFor={option.value}
+            className={`block cursor-pointer text-red-interactive peer-checked:text-white duration-100`}
+          >
+            {option.label}
+          </label>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
 
 export default SortToggle;
