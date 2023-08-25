d6115314fcbc979d5c9d86a6a72c84f4f3d28aef
kylezhao101 <kylezhao101@gmail.com>
2023-08-25 14:45:32 -0700

    Display error text on failed email sign up

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -10,9 +10,15 @@ const Signup = () => {
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
   const onSubmit = async (e) => {
     e.preventDefault();
+
+    if(password.length < 6){
+      setErrorMessage('Password must be at least 6 characters long');
+      return;
+    }
 
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
@@ -24,6 +30,7 @@ const Signup = () => {
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
+        setErrorMessage(errorCode);
         console.log(errorCode, errorMessage);
       });
   };
@@ -68,12 +75,16 @@ const Signup = () => {
           placeholder="Password"
           className='text-lg font-body border-b-2 border-red-interactive bg-transparent text-white outline-none'
         />
+        {password.length > 0 && password.length < 6 && (
+          <p className="text-white">Password must be at least 6 characters long</p>
+        )}
       </div>
 
-      <button type="submit" onClick={onSubmit} className='text-lg text-red-interactive block p-1 pl-3 pr-3 cursor-pointer select-none border-red-interactive border-2 rounded-full hover:bg-red-interactive hover:bg-opacity-20 hover:shadow-red-interactive hover:shadow-md text-center peer-checked:bg-red-interactive peer-checked:text-slate-950 peer-checked:font-bold duration-100 mb-5'>
+      <button type="submit" onClick={onSubmit} className='text-lg text-red-interactive block p-1 pl-3 pr-3 cursor-pointer select-none border-red-interactive border-2 rounded-full hover:bg-red-interactive hover:bg-opacity-20 hover:shadow-red-interactive hover:shadow-md text-center peer-checked:bg-red-interactive peer-checked:text-slate-950 peer-checked:font-bold duration-100'>
         Sign up
       </button>
-      <button type='submit' onClick={callGoogleSignIn} className='text-red-interactive text-lg'>
+      {errorMessage && <p className="text-red-600">{errorMessage}</p>}
+      <button type='submit' onClick={callGoogleSignIn} className='text-red-interactive text-lg mt-5'>
         Sign in with Google
       </button>
     </form>
