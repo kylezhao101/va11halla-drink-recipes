f570c315bdece018950b80c6dbc1737ae1194921
kylezhao101 <kylezhao101@gmail.com>
2023-08-28 16:16:50 -0700

    Revert "Install React Three Fiber"
    This reverts commit c13d2d61ba0513b5aec06d1ff34eaa01ba40c341.

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -53,7 +53,6 @@ const AuthForm = () => {
             const token = credential.accessToken;
             const user = result.user;
             console.log(user)
-            navigate('/');
         }).catch((error) => {
             const errorCode = error.code;
             const errorMessage = error.message;
