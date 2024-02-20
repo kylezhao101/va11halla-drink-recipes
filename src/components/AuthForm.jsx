0faf3d6300024c2b3014915287ab0e9023e06cb1
kylezhao101 <kylezhao101@gmail.com>
2024-02-20 14:09:19 -0800

    Center align login form

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -1,210 +1,251 @@
-import React, { useState } from 'react';
-import '../App.css';
-import { useNavigate } from 'react-router-dom';
-import { GoogleAuthProvider, createUserWithEmailAndPassword } from 'firebase/auth';
-import { auth, provider } from '../util/firebase';
-import { signInWithPopup, signInWithEmailAndPassword, sendPasswordResetEmail } from 'firebase/auth';
-import { collection, doc, setDoc } from 'firebase/firestore';
-import { firestore } from '../util/firebase';
+import React, { useState } from "react";
+import "../App.css";
+import { useNavigate } from "react-router-dom";
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import { auth, provider } from "../util/firebase";
+import {
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  sendPasswordResetEmail,
+} from "firebase/auth";
+import { collection, doc, setDoc } from "firebase/firestore";
+import { firestore } from "../util/firebase";
 
 auth.useDeviceLanguage();
 
 const AuthForm = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [message, setMessage] = useState('');
-    const [isSigningUp, setIsSigningUp] = useState(false);
-    const [isRessettingPassword, setIsResettingPassword] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [isSigningUp, setIsSigningUp] = useState(false);
+  const [isRessettingPassword, setIsResettingPassword] = useState(false);
 
-    const toggleIsSigningUp = () => {
-        setIsSigningUp(!isSigningUp);
-        setIsResettingPassword(false);
-    };
+  const toggleIsSigningUp = () => {
+    setIsSigningUp(!isSigningUp);
+    setIsResettingPassword(false);
+  };
 
-    const toggleIsRessettingPassword = () => {
-        setIsResettingPassword(!isRessettingPassword);
-        setMessage('');
-    };
+  const toggleIsRessettingPassword = () => {
+    setIsResettingPassword(!isRessettingPassword);
+    setMessage("");
+  };
 
-    const onSubmit = async (e) => {
-        e.preventDefault();
+  const onSubmit = async (e) => {
+    e.preventDefault();
 
-        if (isSigningUp && password.length < 6) {
-            setMessage('Password must be at least 6 characters long');
-            return;
-        }
-        if (isSigningUp) {
-            createUserWithEmailAndPassword(auth, email, password)
-                .then((result) => {
-                    const userID = result.user.uid;
-                
-                    setDoc(doc(firestore, 'users', userID), {
-                        Favourites: []
-                    });
-                    navigate('/');
-                })
-                .catch((error) => {
-                    console.log(error)
-                    const errorCode = error.code;
-                    const errorMessage = error.message;
-                    setMessage(errorCode);
-                });
-        } else {
-            signInWithEmailAndPassword(auth, email, password)
-                .then(() => {
-                    navigate('/');
-                })
-                .catch((error) => {
-                    const errorCode = error.code;
-                    const errorMessage = error.message;
-                    setMessage(errorCode);
-                })
-        }
+    if (isSigningUp && password.length < 6) {
+      setMessage("Password must be at least 6 characters long");
+      return;
+    }
+    if (isSigningUp) {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((result) => {
+          const userID = result.user.uid;
 
-        if (isRessettingPassword) {
-            sendPasswordResetEmail(auth, email)
-                .then(() => {
-                    setMessage('Password reset email sent!');
-                    console.log('success')
-                })
-                .catch((error) => {
-                    const errorCode = error.code;
-                    setMessage(errorCode);
-                });
-        }
-    };
+          setDoc(doc(firestore, "users", userID), {
+            Favourites: [],
+          });
+          navigate("/");
+        })
+        .catch((error) => {
+          console.log(error);
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setMessage(errorCode);
+        });
+    } else {
+      signInWithEmailAndPassword(auth, email, password)
+        .then(() => {
+          navigate("/");
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setMessage(errorCode);
+        });
+    }
 
-    const callGoogleSignIn = () => {
-        signInWithPopup(auth, provider)
-            .then((result) => {
-                // This gives you a Google Access Token. You can use it to access the Google API.
-                const credential = GoogleAuthProvider.credentialFromResult(result);
-                const token = credential.accessToken;
-                // The signed-in user info.
-                const userID = result.user.uid;
-                
-                setDoc(doc(firestore, 'users', userID), {
-                    Favourites: []
-                  });
+    if (isRessettingPassword) {
+      sendPasswordResetEmail(auth, email)
+        .then(() => {
+          setMessage("Password reset email sent!");
+          console.log("success");
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          setMessage(errorCode);
+        });
+    }
+  };
 
-            }).catch((error) => {
-                // Handle Errors here.
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                // The email of the user's account used.
-                const email = error.customData.email;
-                // The AuthCredential type that was used.
-                const credential = GoogleAuthProvider.credentialFromError(error);
-                // ...
-            });
+  const callGoogleSignIn = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const userID = result.user.uid;
 
-    };
+        setDoc(doc(firestore, "users", userID), {
+          Favourites: [],
+        });
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
 
-    return (
-        <form className='font-body'>
-            {isRessettingPassword ? (
-                <>
-                    <div className='flex flex-col mb-5'>
-                        <label htmlFor="email-address" className='text-lg text-red-interactive'>Email</label>
-                        <input
-                            type="email"
-                            label="Email address"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            required
-                            placeholder="example@email.com"
-                            className='text-lg font-body border-b-2 border-red-interactive bg-transparent text-white outline-none'
-                        />
-                    </div>
-                    <button type="submit" onClick={onSubmit} className='text-lg text-red-interactive block p-1 pl-3 pr-3 cursor-pointer select-none border-red-interactive border-2 rounded-full hover:bg-red-interactive hover:bg-opacity-20 hover:shadow-red-interactive hover:shadow-md text-center peer-checked:bg-red-interactive peer-checked:text-slate-950 peer-checked:font-bold duration-100'>
-                        Send Email Reset
-                    </button>
-                    {message && <p className="text-white">{message}</p>}
-                    <button type='submit' onClick={toggleIsRessettingPassword} className='text-red-interactive text-base mt-5'>
-                        Back to sign in
-                    </button>
-                </>
-            ) : (
-                <>
-                    <div className='flex gap-2 text-lg text-red-interactive mb-5'>
+  return (
+    <form className="font-body">
+      {isRessettingPassword ? (
+        <>
+          <div className="flex flex-col mb-5">
+            <label
+              htmlFor="email-address"
+              className="text-lg text-red-interactive"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              label="Email address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              placeholder="example@email.com"
+              className="text-lg font-body border-b-2 border-red-interactive bg-transparent text-white outline-none"
+            />
+          </div>
+          <button
+            type="submit"
+            onClick={onSubmit}
+            className="text-lg text-red-interactive block p-1 pl-3 pr-3 cursor-pointer select-none border-red-interactive border-2 rounded-full hover:bg-red-interactive hover:bg-opacity-20 hover:shadow-red-interactive hover:shadow-md text-center peer-checked:bg-red-interactive peer-checked:text-slate-950 peer-checked:font-bold duration-100"
+          >
+            Send Email Reset
+          </button>
+          {message && <p className="text-white">{message}</p>}
+          <button
+            type="submit"
+            onClick={toggleIsRessettingPassword}
+            className="text-red-interactive text-base mt-5"
+          >
+            Back to sign in
+          </button>
+        </>
+      ) : (
+        <>
+          <div className="flex gap-2 text-lg text-red-interactive mb-5">
+            <input
+              type="radio"
+              id="signin"
+              name="authType"
+              checked={!isSigningUp}
+              onChange={toggleIsSigningUp}
+              className="peer hidden"
+            />
+            <label
+              htmlFor="signin"
+              className={`block p-1 pl-3 pr-3 cursor-pointer select-none border-2 border-red-interactive rounded-full hover:bg-red-interactive hover:bg-opacity-20 hover:shadow-red-interactive hover:shadow-md text-center ${
+                isSigningUp ? "text-red-interactive" : "text-white"
+              }`}
+            >
+              Sign In
+            </label>
+            <input
+              type="radio"
+              id="signup"
+              name="authType"
+              checked={isSigningUp}
+              onChange={toggleIsSigningUp}
+              className="peer hidden"
+            />
+            <label
+              htmlFor="signup"
+              className={`block p-1 pl-3 pr-3 cursor-pointer select-none border-2 border-red-interactive rounded-full hover:bg-red-interactive hover:bg-opacity-20 hover:shadow-red-interactive hover:shadow-md text-center ${
+                isSigningUp ? "text-white" : "text-red-interactive"
+              }`}
+            >
+              Sign Up
+            </label>
+          </div>
 
-                        <input
-                            type='radio'
-                            id='signin'
-                            name='authType'
-                            checked={!isSigningUp}
-                            onChange={toggleIsSigningUp}
-                            className='peer hidden'
-                        />
-                        <label
-                            htmlFor='signin'
-                            className={`block p-1 pl-3 pr-3 cursor-pointer select-none border-2 border-red-interactive rounded-full hover:bg-red-interactive hover:bg-opacity-20 hover:shadow-red-interactive hover:shadow-md text-center ${isSigningUp ? 'text-red-interactive' : 'text-white'
-                                }`}
-                        >
-                            Sign In
-                        </label>
-                        <input
-                            type='radio'
-                            id='signup'
-                            name='authType'
-                            checked={isSigningUp}
-                            onChange={toggleIsSigningUp}
-                            className='peer hidden'
-                        />
-                        <label
-                            htmlFor='signup'
-                            className={`block p-1 pl-3 pr-3 cursor-pointer select-none border-2 border-red-interactive rounded-full hover:bg-red-interactive hover:bg-opacity-20 hover:shadow-red-interactive hover:shadow-md text-center ${isSigningUp ? 'text-white' : 'text-red-interactive'
-                                }`}
-                        >
-                            Sign Up
-                        </label>
-                    </div>
+          <div className="flex flex-col mb-5">
+            <label
+              htmlFor="email-address"
+              className="text-lg text-red-interactive"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              label="Email address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              placeholder="example@email.com"
+              className="text-lg font-body border-b-2 border-red-interactive bg-transparent text-white outline-none"
+            />
+          </div>
 
-                    <div className='flex flex-col mb-5'>
-                        <label htmlFor="email-address" className='text-lg text-red-interactive'>Email</label>
-                        <input
-                            type="email"
-                            label="Email address"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            required
-                            placeholder="example@email.com"
-                            className='text-lg font-body border-b-2 border-red-interactive bg-transparent text-white outline-none'
-                        />
-                    </div>
-
-                    <div className='flex flex-col mb-5'>
-                        <label htmlFor="password" className='text-lg text-red-interactive'>Password</label>
-                        <input
-                            type="password"
-                            label="Create password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required
-                            placeholder="Password"
-                            className='text-lg font-body border-b-2 border-red-interactive bg-transparent text-white outline-none'
-                        />
-                        {password.length > 0 && password.length < 6 && (
-                            <p className="text-white">Password must be at least 6 characters long</p>
-                        )}
-                    </div>
-
-                    <button type="submit" onClick={onSubmit} className='text-lg text-red-interactive block p-1 pl-3 pr-3 cursor-pointer select-none border-red-interactive border-2 rounded-full hover:bg-red-interactive hover:bg-opacity-20 hover:shadow-red-interactive hover:shadow-md text-center peer-checked:bg-red-interactive peer-checked:text-slate-950 peer-checked:font-bold duration-100'>
-                        {isSigningUp ? 'Sign Up' : 'Sign In'}
-                    </button>
-                    <button type='submit' onClick={callGoogleSignIn} className='text-red-interactive text-lg mt-5'>
-                        Sign in with Google
-                    </button>
-                    {message && <p className="text-red-600">{message}</p>}
-                    <button type='submit' onClick={toggleIsRessettingPassword} className='block text-red-interactive text-base mt-5'>
-                        Forgot Password?
-                    </button>
-                </>
+          <div className="flex flex-col mb-5">
+            <label htmlFor="password" className="text-lg text-red-interactive">
+              Password
+            </label>
+            <input
+              type="password"
+              label="Create password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              placeholder="Password"
+              className="text-lg font-body border-b-2 border-red-interactive bg-transparent text-white outline-none"
+            />
+            {password.length > 0 && password.length < 6 && (
+              <p className="text-white">
+                Password must be at least 6 characters long
+              </p>
             )}
-        </form>
-    );
+          </div>
+
+          <button
+            type="submit"
+            onClick={onSubmit}
+            className="text-lg text-red-interactive block p-1 pl-3 pr-3 cursor-pointer select-none border-red-interactive border-2 rounded-full hover:bg-red-interactive hover:bg-opacity-20 hover:shadow-red-interactive hover:shadow-md text-center peer-checked:bg-red-interactive peer-checked:text-slate-950 peer-checked:font-bold duration-100"
+          >
+            {isSigningUp ? "Sign Up" : "Sign In"}
+          </button>
+          <button
+            type="submit"
+            onClick={callGoogleSignIn}
+            className="text-red-interactive text-lg mt-5"
+          >
+            Sign in with Google
+          </button>
+          {message && <p className="text-red-600">{message}</p>}
+          <button
+            type="submit"
+            onClick={toggleIsRessettingPassword}
+            className="block text-red-interactive text-base mt-5"
+          >
+            Forgot Password?
+          </button>
+        </>
+      )}
+    </form>
+  );
 };
 
 export default AuthForm;
--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -2,35 +2,34 @@ import AuthForm from "../components/AuthForm";
 import JillsRoom from "../components/JillsRoom";
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
-import { Stats, OrbitControls } from '@react-three/drei'
+import { Stats, OrbitControls } from "@react-three/drei";
 import { Effects } from "@react-three/drei";
 
 function AuthPage() {
-    const target = [0, 4, 0];
-    const cameraPosition = [8, 7, -6];
-    return (
-        <div className="flex">
-            <div className='w-1/3 p-4 lg:p-14'>
-                <AuthForm />
-            </div>
-            <div className="h-screen w-screen">
-                <Canvas 
-                    antialias={true}
-                    pixelRatio={[1, 2]}
-                    rotation={[30, 0, 0]}
-                    camera={{ fov: 70, position: cameraPosition }}>
-                    <Effects antialias={true} />
-                    <Suspense fallback={null}>
-                        <JillsRoom />
-                        <ambientLight color={'white'} intensity={2.0} />
-                    </Suspense>
-                    <OrbitControls target={target} />
-                    <Stats />
-                </Canvas>
-            </div>
-        </div>
-
-    );
+  const target = [0, 4, 0];
+  const cameraPosition = [8, 7, -6];
+  return (
+    <div className="flex">
+      <div className="w-1/3 flex flex-col justify-around p-4 lg:pb-14 lg:pl-14 lg:pr-14">
+        <AuthForm />
+      </div>
+      <div className="h-screen w-screen">
+        <Canvas
+          antialias={true}
+          pixelRatio={[1, 2]}
+          rotation={[30, 0, 0]}
+          camera={{ fov: 70, position: cameraPosition }}
+        >
+          <Effects antialias={true} />
+          <Suspense fallback={null}>
+            <JillsRoom />
+            <ambientLight color={"white"} intensity={2.0} />
+          </Suspense>
+          <OrbitControls target={target} />
+        </Canvas>
+      </div>
+    </div>
+  );
 }
 
 export default AuthPage;
